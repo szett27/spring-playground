34645e832d5bcda2433ec95a7a64dfa895e4637e
Scott <scott.zetterstrom@gmail.com>
2020-07-21 09:54:27 -0600

    Second endpoint is working

--- src/main/java/com/example/demo/CheckpointEndpoints.java
+++ src/main/java/com/example/demo/CheckpointEndpoints.java
@@ -1,4 +1,60 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class CheckpointEndpoints {
+
+    //checkpoint #1 - Camelize
+
+    @GetMapping("/camelize")
+    public String getCamel(@RequestParam("original") String original,
+                           @RequestParam(value = "initialCap", required = false) boolean Cap) {
+        String cameled = "";
+        String[] splitString = original.split("_");
+
+        if (Cap == true) {
+            for (int i = 0; i < splitString.length; i++) {
+                splitString[i] = splitString[i].substring(0, 1).toUpperCase() + splitString[i].substring(1).toLowerCase();
+            }
+        } else {
+            for (int i = 1; i < splitString.length; i++) {
+                splitString[i] = splitString[i].substring(0, 1).toUpperCase() + splitString[i].substring(1).toLowerCase();
+            }
+        }
+
+        cameled = String.join("", splitString);
+
+        return cameled;
+    }
+
+
+    //checkpoint #2 - Redact
+    @GetMapping("/redact")
+    public String redact(@RequestParam(value = "original", required = true) String original,
+                         @RequestParam(value = "badWord") List<String> badwords) {
+        String redacted = "";
+        String[] sentence = original.split(" ");
+        for(String word: badwords){
+            for(int j =0; j < sentence.length; j++){
+                if(sentence[j].equals(word)){
+                    String redact = "";
+                    for(int i =0; i < word.split("").length; i++){
+                        redact += "*";
+                    }
+                    sentence[j] = redact;
+
+                }
+
+            }
+        }
+
+        redacted = String.join(" ", sentence);
+        return redacted;
+    }
+
 }
--- src/main/java/com/example/demo/SpringFlight.java
+++ src/main/java/com/example/demo/SpringFlight.java
@@ -1,4 +1,88 @@
 package com.example.demo;
 
-public class SpringFlight {
-}
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+
+public class SpringFlight{
+
+    @GetMapping("/flights/flight")
+    public Flight getflights() {
+//        Flight.Passenger Scott = new Flight.Passenger("Scott", "Ze");
+//        Flight.Ticket first = new Flight.Ticket(Scott, "150");
+//        Flight flight = new Flight(first, new Date(1984,05,27));
+            Flight flight = new Flight();
+
+
+        return flight;
+    }
+
+    public static class Flight{
+        private Date departs;
+        private Ticket ticket;
+        private List<Ticket> tickets;
+
+        Flight(){
+
+        }
+
+        Flight(Ticket ticket, Date departs){
+            this.ticket = ticket;
+            this.departs = departs;
+        }
+
+
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        public Date getDeparts() { return departs; }
+        public void setDeparts(Date dateTime) { this.departs = dateTime;}
+
+        public List<Ticket> getTickets() { return tickets;}
+        public void setTickets(List<Ticket> tickets) { this.tickets = tickets;}
+
+        public Ticket getTicket(){return this.ticket;}
+
+
+        static class Passenger {
+            private final String firstname;
+            private final String lastname;
+
+            Passenger(String firstname, String lastname){
+                this.firstname = firstname;
+                this.lastname = lastname;
+            }
+
+            public String getFirstName() {return firstname; }
+            public String getLastName(){ return lastname;}
+
+        }
+
+        static class Ticket{
+
+                private final Passenger passenger;
+            private final String price;
+
+                Ticket(Passenger passenger, String price){
+                    this.passenger = passenger;
+                    this.price = price;
+                }
+
+            public Passenger getPassenger(){ return passenger;}
+            public String getPrice() { return price; }
+
+        }
+        }
+    }
+
+
+
+
+
+
